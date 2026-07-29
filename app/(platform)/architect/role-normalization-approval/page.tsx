9a02bba678b16ede498711fe99d52e168fe683dd
bvrb3r <bvrb3r@gmail.com>
2026-07-28 21:00:16 -0400

    feat(pr28): expose role approval page

---
+++ app/(platform)/architect/role-normalization-approval/page.tsx
@@ -0,0 +1,7 @@
+import { RoleNormalizationApprovalSurface } from "@/components/architect/role-normalization-approval-surface";
+
+export const dynamic = "force-dynamic";
+
+export default function ArchitectRoleNormalizationApprovalPage() {
+  return <RoleNormalizationApprovalSurface />;
+}
